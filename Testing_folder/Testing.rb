cd50171e971ad785e362f36f4756779572511a75
Lewis MacNee <lewis.macnee@hotmail.com>
2016-11-25 09:15:56 +0000

    two for one deal working

--- Testing_folder/Testing.rb
+++ Testing_folder/Testing.rb
@@ -1,11 +1,22 @@
 require('pry-byebug')
 
-def percentage_off(price, percent)
+# def percentage_off(price, percent)
 
-  percent_off = percent/100.0
-  deal_price = percent_off * price
+#   percent_off = percent/100.0
+#   deal_price = percent_off * price
+#   return deal_price
+
+# end
+
+# puts percentage_off(100, 75)
+
+def two_for_one(price1, price2)
+
+  prices = []
+  prices << price1
+  prices << price2
+  deal_price = prices.max
   return deal_price
-
 end
 
-puts percentage_off(100, 75)
+puts two_for_one(20, 26)
